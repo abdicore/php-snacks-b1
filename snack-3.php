8e27ebe8b6aa02af09a74fdca6087c7739d846da
abdicore <abdirhamanmohamed@gmail.com>
2024-10-04 12:52:29 +0200

    forech sbagliato

---
+++ snack-3.php
@@ -0,0 +1,60 @@
+<?php
+ 
+ 
+        $teachers = [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+            ];
+        $pm = [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+;
+ 
+?>
+
+
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <!-- form -->
+     <main class="container">
+        <section class="row">
+            <div class="col-12">
+                <ul>
+                    <?php foreach ($teacher as $teachers)?>
+                    <li>
+                    <?php echo $teacher['name']; ?>
+                    </li>
+                </ul>
+               
+            
+               
+            </div>
+        </section>
+     </main>
+    
+</body>
+</html>
